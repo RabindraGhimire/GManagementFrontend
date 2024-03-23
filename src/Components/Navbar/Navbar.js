2298cecda16acd95397b1d921a41d31659fd0a56
RabindraGhimire <rabindraghimire.rg@gmail.com>
2024-03-23 23:22:34 +0545

    ReactFrontendGymManagementFirstCommit

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Route, Router, Routes } from 'react-router-dom'
+import Homepage from '../../Pages/Homepage'
+
+const Navbar = () => {
+  return (
+    <div>
+        <h1>This is the navbar</h1>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/Pages/Userspage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Userspage = () => {
+  return (
+    <div>
+      <h1>This is the Userspage</h1>
+    </div>
+  )
+}
+
+export default Userspage
